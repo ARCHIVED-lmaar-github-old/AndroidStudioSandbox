17d1a62199c8e5a7ad2961ae765b17b7102d8524
Laurence Maar <laurencemaar@gmail.com>
2019-11-05 17:34:30 -0800

    Updated

--- app/src/main/java/com/example/androidstudiosandbox/TabbedActivity.java
+++ app/src/main/java/com/example/androidstudiosandbox/TabbedActivity.java
@@ -1,9 +1,11 @@
 /*
  * Android Studio Demo
  * -----
- * This is demo shows basic Android functionality such as having multiple Activities and
+ * This is a demo which shows basic Android functionality such as having multiple Activities and
  * a Tabbed Activity composed of multiple fragments.
- * Each fragment has some kind of unique functionality.
+ *
+ * Each fragment has some kind of unique functionality - Random Number Generator, Sevice Generation,
+ * Mock Video Game Battle, etc.
  * -----
  * Laurence Maar
  * https://www.linkedin.com/in/laurencemaar/
